fc44a8fb9fd60a5e7492accabcc5b8f02dda8ec8
Highperial <antishadowss@gmail.com>
2025-01-22 21:24:49 +0100

    expert coords pick logic fix

--- mini_games/buildMarines/dagger_expert_policy.py
+++ mini_games/buildMarines/dagger_expert_policy.py
@@ -5,16 +5,16 @@ from stable_baselines3.common import policies
 
 from build_marines_actions import *
 
-# I cannot for the life of me find where unit order ids are defined so this will have to do
 DEPOT_CHASE_BUILD_ID = 222
 BARRACK_CHASE_BUILD_ID = 185
 DEPOT_BUILD_ORDER_ID = 362
 
 BUILD_ORDERS = [DEPOT_CHASE_BUILD_ID, BARRACK_CHASE_BUILD_ID, DEPOT_BUILD_ORDER_ID]
 
-# Don't even think these are defined anywhere
 DEPOT_DIAMETER = 8
 BARRACK_DIAMETER = 12
+DEPOT_RADIUS = 4
+BARRACK_RADIUS = 6
 
 
 class ExpertPolicy(policies.BasePolicy, ABC):
@@ -26,6 +26,8 @@ class ExpertPolicy(policies.BasePolicy, ABC):
         self.last_depot = (64, 64)  # Map height is 64
         self.expected_barrack_count = 0
         self.expected_depot_count = 0
+
+        print("---EXPERT INIT---")
 
     def _predict(self, obs, deterministic=False):
         observation = self.env.current_obs.observation
@@ -87,19 +89,17 @@ class ExpertPolicy(policies.BasePolicy, ABC):
 
     def build_supply_depot(self, observation):
         coords = self.get_next_depot_coords()
-        self.last_depot = coords
 
-        while are_occupied_coords_depot(observation, coords):
+        while not are_unoccupied_coords_depot(observation, coords):
             coords = self.get_next_depot_coords()
 
         return build_action(2, coords)
 
     def build_barracks(self, observation):
         coords = self.get_next_barracks_coords()
-        self.last_barracks = coords
 
-        while are_occupied_coords_barrack(observation, coords):
-            coords = self.get_next_barracks_coords()
+        while not are_unoccupied_coords_barrack(observation, coords):
+            coords = self.get_next_barracks_coords
 
         return build_action(1, coords)
 
@@ -107,12 +107,16 @@ class ExpertPolicy(policies.BasePolicy, ABC):
         coords = (self.last_depot[0] - DEPOT_DIAMETER, self.last_depot[1])
         if coords[0] < 1:
             coords = (64 - DEPOT_DIAMETER, max(coords[1] - DEPOT_DIAMETER, 1))
+
+        self.last_depot = coords
         return coords
 
     def get_next_barracks_coords(self):
         coords = (self.last_barracks[0] + BARRACK_DIAMETER, self.last_barracks[1])
         if coords[0] > 84:
             coords = (10, min(coords[1] + BARRACK_DIAMETER, 84))
+
+        self.last_barracks = coords
         return coords
 
 
@@ -156,22 +160,23 @@ def is_selected_unit_building(obs, unit_type):
     return unit.order_id_0 in BUILD_ORDERS or unit.order_id_1 in BUILD_ORDERS
 
 
-def are_occupied_coords_barrack(obs, coords):
-    # Safety checks for coords might be redundant, but don't hurt
-    for x in range(coords[0], min(coords[0] + BARRACK_DIAMETER, 84)):
-        for y in range(coords[1], min(coords[1] + BARRACK_DIAMETER, 64)):
+def are_unoccupied_coords_barrack(obs, coords):
+    # Invert because feature screen is in matrix
+    # Out of bounds safety checks are redundant but don't hurt
+    for y in range(max(coords[0] - BARRACK_RADIUS, 1), min(coords[0] + BARRACK_RADIUS, 84)):
+        for x in range(max(coords[0] - BARRACK_RADIUS, 1), min(coords[1] + BARRACK_RADIUS, 64)):
             screen_unit = obs.feature_screen.unit_type[x][y]
-            if screen_unit == 0 or (screen_unit != units.Terran.SCV and screen_unit != units.Terran.Marine):
+            if screen_unit != 0 and (screen_unit != units.Terran.SCV and screen_unit != units.Terran.Marine):
                 return False
 
     return True
 
 
-def are_occupied_coords_depot(obs, coords):
-    for x in range(max(coords[0] - DEPOT_DIAMETER, 1), coords[0]):
-        for y in range(max(coords[1] - DEPOT_DIAMETER, 1), coords[1]):
+def are_unoccupied_coords_depot(obs, coords):
+    for y in range(max(coords[0] - DEPOT_RADIUS, 1), min(coords[0] + DEPOT_RADIUS, 84)):
+        for x in range(max(coords[1] - DEPOT_RADIUS, 1), min(coords[1] + DEPOT_RADIUS, 64)):
             screen_unit = obs.feature_screen.unit_type[x][y]
-            if screen_unit == 0 or (screen_unit != units.Terran.SCV and screen_unit != units.Terran.Marine):
+            if screen_unit != 0 and (screen_unit != units.Terran.SCV and screen_unit != units.Terran.Marine):
                 return False
 
     return True
